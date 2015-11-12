d5c58a0cbe2f48e18505dcf2c16cd0c5dc6835b0
Demo User <alex.oct.monti@gmail.com>
2015-11-12 19:22:19 +0000

    Added SysFS support basis to bbb/modules/rap using kobject

--- software/bbb/modules/rap/rap.c
+++ software/bbb/modules/rap/rap.c
@@ -24,6 +24,10 @@ MODULE_VERSION("");
 
 struct rap_device
 {
+    struct kobject* kobj;
+    struct attribute_group kobj_attr_group;
+    uint8_t regval;
+
     struct spi_device* spi_device;
     struct list_head list;
 };
@@ -39,8 +43,93 @@ static DEFINE_MUTEX(device_list_lock);
 /*** Prototypes ***/
 /******************/
 
+static uint8_t spi_write_reg(struct rap_device* rap, uint8_t reg, uint8_t val);
+static ssize_t debug_show(struct kobject* kobj, struct kobj_attribute* attr, char* buf);
+static ssize_t debug_store(struct kobject* kobj, struct kobj_attribute* attr, const char* buf, size_t size);
+static struct rap_device* kobj_rap_device(struct kobject* kobj);
 static int rap_driver_probe(struct spi_device* spi);
 static int rap_driver_remove(struct spi_device* spi);
+
+/**************************/
+/*** SPI RAP primitives ***/
+/**************************/
+
+static uint8_t spi_write_reg(struct rap_device* rap, uint8_t reg, uint8_t val)
+{
+    struct spi_transfer t[2];
+    struct spi_message m;
+    uint8_t txbuf[2];
+    uint8_t rxbuf;
+
+    spi_message_init(&m);
+
+    memset(t, 0, sizeof(t));
+
+    txbuf[0] = 0xFE;
+    txbuf[1] = reg;
+
+    t[0].tx_buf = txbuf;
+    t[0].len = 2;
+    spi_message_add_tail(&t[0], &m);
+
+    t[1].rx_buf = &rxbuf;
+    t[1].len = 1;
+    spi_message_add_tail(&t[1], &m);
+
+    // spin_lock_irqsave(&spilock, spilockflags);
+    spi_sync(rap->spi_device, &m);
+    // spin_unlock_irqrestore(&spilock, spilockflags);
+
+    return rxbuf;
+}
+
+/************************/
+/*** kobject bindings ***/
+/************************/
+
+static ssize_t debug_show(struct kobject* kobj, struct kobj_attribute* attr, char* buf)
+{
+    struct rap_device* rap;
+
+    rap = kobj_rap_device(kobj);
+    if (!rap)
+        return -ENOMEM; //TODO: correct error ?
+    
+    return sprintf(buf, "0x%02X\n", rap->regval);
+}
+
+static ssize_t debug_store(struct kobject* kobj, struct kobj_attribute* attr, const char* buf, size_t size)
+{
+    struct rap_device* rap;
+
+    rap = kobj_rap_device(kobj);
+    if (!rap)
+        return -ENOMEM; //TODO: correct error ?
+
+    rap->regval = spi_write_reg(rap, 0xAB, 0xCD);
+
+    return size;
+}
+
+static struct kobj_attribute debug_attr = __ATTR(debug, 0666, debug_show, debug_store);
+
+static struct attribute* rap_attrs[] =
+{
+    &debug_attr.attr,
+    0
+};
+
+static struct rap_device* kobj_rap_device(struct kobject* kobj)
+{
+    struct rap_device* rap;
+    list_for_each_entry(rap, &device_list, list)
+    {
+        if (rap->kobj == kobj)
+            return rap;
+    }
+
+    return 0;
+}
 
 /**************************/
 /*** Device match table ***/
@@ -73,6 +162,7 @@ static struct spi_driver rap_driver =
 
 static int rap_driver_probe(struct spi_device* spi)
 {
+    int err;
     struct rap_device* rap;
 
     printk(KERN_INFO "rap_driver_probe()\n");
@@ -82,12 +172,28 @@ static int rap_driver_probe(struct spi_device* spi)
     if (!rap)
         return -ENOMEM;
 
-    // Add it in our device list
-    INIT_LIST_HEAD(&rap->list);
-    list_add(&rap->list, &device_list);
+    // Create kobject entry
+    rap->kobj = kobject_create_and_add("repico_rap", kernel_kobj);
+    if (!rap->kobj)
+    {
+        printk(KERN_ALERT "repico_rap: failed to create kobject\n");
+        kfree(rap);
+        return -ENOMEM;
+    }
 
-    // Create sysfs kobj
-    // Create sysfs attr group
+    // Setup an unnamed attribute group
+    rap->kobj_attr_group.name = 0;
+    rap->kobj_attr_group.attrs = rap_attrs;
+
+    // And associated SysFS entries
+    err = sysfs_create_group(rap->kobj, &rap->kobj_attr_group);
+    if(err)
+    {
+        printk(KERN_ALERT "repico_rap: failed to create sysfs group\n");
+        kobject_put(rap->kobj);
+        kfree(rap);
+        return err;
+    }
 
     // Initialize the SPI device
     spi->max_speed_hz = 500000;
@@ -97,6 +203,10 @@ static int rap_driver_probe(struct spi_device* spi)
     // Add driver-specific data to the SPI device
     //   so we can find ourselves in driver_remove for example
     spi_set_drvdata(spi, rap);
+
+    // Add the RAP device in our device list
+    INIT_LIST_HEAD(&rap->list);
+    list_add(&rap->list, &device_list);
 
     return 0;
 }
@@ -109,6 +219,9 @@ static int rap_driver_remove(struct spi_device* spi)
 
     // Get corresponding RAP device
     rap = spi_get_drvdata(spi);
+
+    // Release SysFS entries
+    kobject_put(rap->kobj);
 
     // Remove the RAP device from list and free allocated memory
     mutex_lock(&device_list_lock);
