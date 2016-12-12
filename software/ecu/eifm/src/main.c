853999af249b43e77760177feb8972a16ab1aa9a
Alexandre Monti <amont65@hotmail.fr>
2016-12-12 20:28:31 +0100

    Changed SPI word size to 16, works with RAP BBB module

--- software/ecu/eifm/src/main.c
+++ software/ecu/eifm/src/main.c
@@ -22,119 +22,165 @@ void spi_disable(SPI_TypeDef* port)
         RCC->APB1ENR &= ~(0x01 << 15);
 }
 
-unsigned int pos = 0;
-uint8_t buf[8];
-
 int main()
 {
     itm_init();
-
-    // Enable GPIOs
-    gpio_enable(GPIOB);
-    
-    // Configure GPIOs to AF5
-    gpio_init_af(GPIOB, 12);
-    gpio_init_hs(GPIOB, 12);
-    gpio_init_pp(GPIOB, 12);
-    gpio_init_nopupd(GPIOB, 12);
-    gpio_select_af(GPIOB, 12, 5);
-    
-    gpio_init_af(GPIOB, 13);
-    gpio_init_hs(GPIOB, 13);
-    gpio_init_pp(GPIOB, 13);
-    gpio_init_nopupd(GPIOB, 13);
-    gpio_select_af(GPIOB, 13, 5);
-    
-    gpio_init_af(GPIOB, 14);
-    gpio_init_hs(GPIOB, 14);
-    gpio_init_pp(GPIOB, 14);
-    gpio_init_nopupd(GPIOB, 14);
-    gpio_select_af(GPIOB, 14, 5);
-    
-    gpio_init_af(GPIOB, 15);
-    gpio_init_hs(GPIOB, 15);
-    gpio_init_pp(GPIOB, 15);
-    gpio_init_nopupd(GPIOB, 15);
-    gpio_select_af(GPIOB, 15, 5);
 
     // Enable peripheral clock
     spi_enable(SPI2);
 
     // DFF = 0 (8 bit frames)
-    SPI2->CR1 &= ~(0x01 << 11);
+    SPI2->CR1 |= (0x01 << 11);
+
     // CPOL = 0 (SCK 0 when idle)
     SPI2->CR1 &= ~(0x01 << 1);
-    // CPHA = 0 (firct clock transition is first data capture)
+    // CPHA = 1 (second clock transition is first data capture)
     SPI2->CR1 &= ~(0x01 << 0);
+
     // LSBFIRST = 0
     SPI2->CR1 &= ~(0x01 << 7);
+
     // SSM = 0 (hardware NSS)
     SPI2->CR1 |= (0x01 << 9);
     // SSI = 0 (slave enabled)
     SPI2->CR1 &= ~(0x01 << 8);
+
     // MSTR = 0 (slave mode)
     SPI2->CR1 &= ~(0x01 << 2);
 
     // SPE = 1 (enable SPI2)
     SPI2->CR1 |= (0x01 << 6);
 
-    // TXEIE = 1
-    SPI2->CR2 |= (0x01 << 7);
+    // TXEIE = 0
+    SPI2->CR2 &= ~(0x01 << 7);
     // RXNEIE = 1
     SPI2->CR2 |= (0x01 << 6);
     // ERRIE = 1
-    //SPI2->CR2 |= (0x01 << 5);
+    SPI2->CR2 |= (0x01 << 5);
 
     // SPI2 is interrupt #36
     NVIC->ISER[1] |= (0x01 << 4);
     // Set max priority
     NVIC->IP[36] = (0 << 4);
 
-    for (;;)
-    {
-        /*uint16_t sr = SPI2->SR;
+    // SPI2->DR = 0x13;
 
-        if (sr & (0x01 << 0))
-        {
-            buf[pos++] = SPI2->DR;
-        }
+    // Enable GPIOs
+    gpio_enable(GPIOB);
+    gpio_enable(GPIOC);
+    
+    // Configure GPIOs to AF5
+    gpio_init_af(GPIOB, 10);
+    gpio_select_af(GPIOB, 10, 5);
+    
+    gpio_init_af(GPIOC, 2);
+    gpio_select_af(GPIOC, 2, 5);
+    
+    gpio_init_af(GPIOC, 3);
+    gpio_select_af(GPIOC, 3, 5);
 
-        if (pos >= sizeof(buf)) {
-            for (int i = 0; i < sizeof(buf); ++i)
-                itm_printf("0x%02X ", buf[i]);
-            itm_printf("\n");
-            pos = 0;
-        }
-        
-        if (sr & (0x01 << 1))
-        {
-            SPI2->DR = 0xCA;
-        }*/
-    }
+    for (;;);
 }
+
+enum
+{
+    RAP_NONE = 0x0000,
+    RAP_WRITE = 0x0001,
+    RAP_READ = 0x0002
+};
+
+enum
+{
+    SYNC_RCVE,
+    SYNC_SEND,
+    SYNCED,
+    COMMAND
+} rap_state = SYNC_RCVE;
+
+uint16_t rap_command = RAP_NONE;
+
+struct
+{
+    enum
+    {
+        WRITE_GET_REG_ID,
+        WRITE_GET_REG_SIZE,
+        WRITE_GET_REG_DATA
+    } state;
+
+    uint16_t reg_id;
+    uint16_t size;
+    uint16_t data[256];
+    uint16_t pos;
+} rap_write_cmd;
 
 void irq_spi2_handler()
 {
     // Clear interrupt
     NVIC->ICPR[1] |= (0x01 << 4);
 
-    uint16_t sr = SPI2->SR;
+    volatile uint16_t sr = SPI2->SR;
 
-    // RXNE
-    if (sr & (0x01 << 0))
+    if ((sr & (0x01 << 3)) || (sr & (0x01 << 6))) // UDR == 1 or OVR == 1
     {
-        buf[pos++] = SPI2->DR;
+        itm_printf("Error: 0x%08X\n", sr);
 
-        if (pos >= sizeof(buf)) {
-            for (int i = 0; i < sizeof(buf); ++i)
-                itm_printf("0x%02X ", buf[i]);
-            itm_printf("\n");
-            pos = 0;
-        }
+        rap_state = SYNC_RCVE;
+
+        // Clear error flag using this software sequence
+        volatile uint8_t dr = SPI2->DR;
+        sr = SPI2->SR;
+        return;
     }
-    // TXE
-    else if (sr & (0x01 << 1))
+    else if (sr & (0x01 << 0)) // RXNE == 1
     {
-        // SPI2->DR = 0xAB;
+        volatile uint16_t dr = SPI2->DR;
+
+        itm_printf("%04X\n", dr);
+
+        switch (rap_state)
+        {
+            case SYNC_RCVE:
+                SPI2->DR = dr;
+                rap_state = SYNC_SEND;
+                break;
+
+            case SYNC_SEND:
+                SPI2->DR = 0x00;
+                rap_state = SYNCED;
+                break;
+
+            case SYNCED:
+                rap_command = dr;
+                rap_state = COMMAND;
+                rap_write_cmd.state = WRITE_GET_REG_ID;
+                break;
+
+            case COMMAND:
+                if (rap_command == RAP_WRITE)
+                {
+                    if (rap_write_cmd.state == WRITE_GET_REG_ID)
+                    {
+                        rap_write_cmd.reg_id = dr;
+                        rap_write_cmd.state = WRITE_GET_REG_SIZE;
+                    }
+                    else if (rap_write_cmd.state == WRITE_GET_REG_SIZE)
+                    {
+                        rap_write_cmd.size = dr;
+                        rap_write_cmd.state = WRITE_GET_REG_DATA;
+                        rap_write_cmd.pos = 0;
+                    }
+                    else if (rap_write_cmd.state == WRITE_GET_REG_DATA)
+                    {
+                        rap_write_cmd.data[rap_write_cmd.pos++] = dr;
+                        if (rap_write_cmd.pos >= rap_write_cmd.size)
+                        {
+                            rap_state = SYNC_RCVE;
+                            itm_printf("Write register %04X with %04X bytes\n", rap_write_cmd.reg_id, rap_write_cmd.size);
+                        }
+                    }
+                }
+                break;
+        }
     }
 }
