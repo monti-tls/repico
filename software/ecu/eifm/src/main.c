5006d9adbfd53b02a556cfe7419a937773673fa6
Alexandre Monti <amont65@hotmail.fr>
2016-12-17 11:00:04 +0100

    RAP/SPI R/W works :)

--- software/ecu/eifm/src/main.c
+++ software/ecu/eifm/src/main.c
@@ -2,66 +2,121 @@
 #include "itm.h"
 #include "gpio.h"
 
-void spi_enable(SPI_TypeDef* port)
+enum
 {
-    if (port == SPI1)
-        RCC->APB2ENR |= (0x01 << 12);
-    else if (port == SPI2)
-        RCC->APB1ENR |= (0x01 << 14);
-    else if (port == SPI3)
-        RCC->APB1ENR |= (0x01 << 15);
+    RAP_CMD_WRITE = 0x01,
+    RAP_CMD_READ = 0x02,
+
+    RAP_STATUS_OK = 0xF0,
+    RAP_STATUS_INVALID_CMD = 0xF1, // invalid command byte
+    RAP_STATUS_INVALID_REG = 0xF2, // no such register id
+    RAP_STATUS_INVALID_ACC = 0xF3  // invalid reg access (R/W issue)
+};
+
+struct rap_register
+{
+    uint8_t id;
+    
+    enum
+    {
+        RAP_REG_R = 0x01,
+        RAP_REG_W = 0x02
+    } attrs;
+
+    enum
+    {
+        RAP_REG_BUFFER,
+        RAP_REG_HANDLER
+    } mode;
+
+    void* buffer;
+    int len;
+    uint8_t (*read)(uint8_t* data);
+    void (*write)(uint8_t* data, uint8_t size);
+};
+
+struct rap_device
+{
+    SPI_TypeDef* spi;
+    struct
+    {
+        GPIO_TypeDef* port;
+        int pin;
+    } spi_nss;
+    struct rap_register* registers;
+
+    uint8_t rx[300]; // hold 256 bytes of payload + command
+    unsigned int rx_len;
+
+    uint8_t tx[300];
+    unsigned int tx_len;
+    unsigned int tx_ptr;
+
+    enum
+    {
+        RAP_IDLE,
+        RAP_COMMAND
+    } state;
+};
+
+struct rap_register* rap_find_register(struct rap_device* rap, uint8_t reg)
+{
+    for (struct rap_register* r = rap->registers; r->id; ++r)
+    {
+        if (r->id == reg)
+            return r;
+    }
+
+    return 0;
 }
 
-void spi_disable(SPI_TypeDef* port)
+/** Definitions **/
+
+uint8_t reg0_read(uint8_t* data)
 {
-    if (port == SPI1)
-        RCC->APB2ENR &= ~(0x01 << 12);
-    else if (port == SPI2)
-        RCC->APB1ENR &= ~(0x01 << 14);
-    else if (port == SPI3)
-        RCC->APB1ENR &= ~(0x01 << 15);
+    data[0] = 0xCA;
+    data[1] = 0xFE;
+    return 2;
 }
+
+void reg0_write(uint8_t* data, uint8_t len)
+{
+    itm_printf("Write reg0 ");
+    for (int i = 0; i < len; ++i)
+        itm_printf("%02X ", data[i]);
+    itm_printf("\n");
+}
+
+int reg1;
+
+struct rap_register registers[] =
+{
+    {
+        0xAB,
+        RAP_REG_R | RAP_REG_W,
+        RAP_REG_HANDLER,
+        0, 0,
+        &reg0_read,
+        &reg0_write
+    },
+    {
+        0xAC,
+        RAP_REG_R | RAP_REG_W,
+        RAP_REG_BUFFER,
+        &reg1,
+        sizeof(reg1),
+        0, 0
+    },
+    { 0x00 }
+};
+
+/** Locals **/
+
+struct rap_device rap;
 
 int main()
 {
     itm_init();
-
-    // Enable peripheral clock
-    spi_enable(SPI2);
-
-    // DFF = 0 (8 bit frames)
-    SPI2->CR1 &= ~(0x01 << 11);
-
-    // CPOL = 0 (SCK 0 when idle)
-    SPI2->CR1 &= ~(0x01 << 1);
-    // CPHA = 0 (first clock transition is first data capture)
-    SPI2->CR1 |= (0x01 << 0);
-
-    // LSBFIRST = 0
-    SPI2->CR1 &= ~(0x01 << 7);
-
-    // SSM = 0 (hardware NSS)
-    SPI2->CR1 &= ~(0x01 << 9);
-    // SSI = 0 (slave enabled)
-    // SPI2->CR1 &= ~(0x01 << 8);
-
-    // MSTR = 0 (slave mode)
-    SPI2->CR1 &= ~(0x01 << 2);
-
-    // SPE = 1 (enable SPI2)
-    SPI2->CR1 |= (0x01 << 6);
-
-    // TXEIE = 0
-    SPI2->CR2 &= ~(0x01 << 7);
-    // RXNEIE = 1
-    SPI2->CR2 &= ~(0x01 << 6);
-    // ERRIE = 1
-    SPI2->CR2 &= ~(0x01 << 5);
-
-    // SPI2 is interrupt #36
-    NVIC->ISER[1] |= (0x01 << 4);
-    // Set max priority
-    NVIC->IP[36] = (0 << 4);
 
     // Enable GPIOs
     gpio_enable(GPIOB);
@@ -80,101 +135,162 @@ int main()
     gpio_init_af(GPIOC, 3);
     gpio_select_af(GPIOC, 3, 5);
 
-    uint8_t tx[] = { 0xF1, 0xF2, 0xF3 };
-    uint8_t rx[sizeof(tx)];
-    int ptx = 0;
-    int prx = 0;
+    // Enable SPI2 clock
+    RCC->APB1ENR |= (0x01 << 14);
+
+    // DFF = 0 (8 bit frames)
+    SPI2->CR1 &= ~(0x01 << 11);
+
+    // CPOL = 0 (SCK 0 when idle)
+    SPI2->CR1 &= ~(0x01 << 1);
+    // CPHA = 0 (first clock transition is first data capture)
+    SPI2->CR1 |= (0x01 << 0);
+
+    // LSBFIRST = 0
+    SPI2->CR1 &= ~(0x01 << 7);
+    // SSM = 0 (hardware NSS)
+    SPI2->CR1 &= ~(0x01 << 9);
+
+    // MSTR = 0 (slave mode)
+    SPI2->CR1 &= ~(0x01 << 2);
+
+    // TXEIE = 1
+    SPI2->CR2 |= (0x01 << 7);
+    // RXNEIE = 1
+    SPI2->CR2 |= (0x01 << 6);
+    // ERRIE = 1
+    SPI2->CR2 |= (0x01 << 5);
+
+    // SPI2 is interrupt #36
+    NVIC->ISER[1] |= (0x01 << 4);
+    // Set max priority
+    NVIC->IP[36] = (0 << 4);
+
+    // SPE = 1 (enable SPI2)
+    SPI2->CR1 |= (0x01 << 6);
+
+    rap.spi = SPI2;
+    rap.spi_nss.port = GPIOB;
+    rap.spi_nss.pin = 12;
+    rap.registers = &registers[0];
+    rap.state = RAP_IDLE;
+    rap.rx_len = 0;
+    rap.tx_len = 0;
+    rap.tx_ptr = 0;
 
     for (;;)
     {
-        SPI2->DR = tx[ptx++];
-        
-        while (ptx < sizeof(tx))
+        if (gpio_read(rap.spi_nss.port, rap.spi_nss.pin) && rap.rx_len)
         {
-            while (!(SPI2->SR & 2));
-            SPI2->DR = tx[ptx++];
-            while (!(SPI2->SR & 1));
-            rx[prx++] = SPI2->DR;
+            switch (rap.state)
+            {
+                case RAP_IDLE:
+                    rap.state = RAP_COMMAND;
+                    
+                    rap.tx_len = 1;
+                    rap.tx[0] = rap.rx[0]; // sync byte
+                    
+                    if (rap.rx[1] == RAP_CMD_WRITE)
+                    {
+                        struct rap_register* reg = rap_find_register(&rap, rap.rx[2]);
+
+                        uint8_t status = RAP_STATUS_OK;
+                        if (!reg)
+                            status = RAP_STATUS_INVALID_REG;
+                        else if (!(reg->attrs & RAP_REG_W))
+                            status = RAP_STATUS_INVALID_ACC;
+                        else
+                        {
+                            if (reg->mode == RAP_REG_BUFFER)
+                            {
+                                if (rap.rx[3]  != reg->len)
+                                    status = RAP_STATUS_INVALID_ACC;
+                                else
+                                    __builtin_memcpy(reg->buffer, &rap.rx[4], rap.rx[3]);
+                            }
+                            else
+                                (*reg->write)(&rap.rx[4], rap.rx[3]);
+                        }
+
+                        rap.tx[rap.tx_len++] = status;
+                        rap.tx[rap.tx_len++] = 0; // 0 more bytes to read
+                    }
+                    else if (rap.rx[1] == RAP_CMD_READ)
+                    {
+                        struct rap_register* reg = rap_find_register(&rap, rap.rx[2]);
+
+                        if (!reg || !(reg->attrs & RAP_REG_R))
+                        {
+                            rap.tx[rap.tx_len++] = reg ? RAP_STATUS_INVALID_ACC : RAP_STATUS_INVALID_REG;
+                            rap.tx[rap.tx_len++] = 0;
+                        }
+                        else
+                        {
+                            rap.tx[rap.tx_len++] = RAP_STATUS_OK;
+                            uint8_t* len = &rap.tx[rap.tx_len++];
+
+                            if (reg->mode == RAP_REG_BUFFER)
+                            {
+                                __builtin_memcpy(&rap.tx[rap.tx_len], reg->buffer, reg->len);
+                                *len = reg->len;
+                            }
+                            else
+                                *len = (*reg->read)(&rap.tx[rap.tx_len]);
+
+                            rap.tx_len += *len;
+                        }
+                    }
+                    else
+                    {
+                        rap.tx[rap.tx_len++] = RAP_STATUS_INVALID_CMD;
+                        rap.tx[rap.tx_len++] = 0; // 0 more bytes to read
+                    }
+                    break;
+
+                case RAP_COMMAND:
+                    if (!rap.tx_len)
+                        rap.state = RAP_IDLE;
+                    break;
+            }
+
+            rap.rx_len = 0;
         }
-
-        while (!(SPI2->SR & 1));
-            rx[prx++] = SPI2->DR;
-
-        while (!(SPI2->SR & 2));
-        while ((SPI2->SR & (1 << 7)));
-
-        for (int i = 0; i < sizeof(tx); ++i)
-            itm_printf("%02X ", rx[i]);
-        itm_printf("\n");
-
-        ptx = 0;
-        prx = 0;
     }
-
-    for (;;);
 }
-
-enum
-{
-    RAP_NONE = 0x0000,
-    RAP_WRITE = 0x0001,
-    RAP_READ = 0x0002
-};
-
-enum
-{
-    SYNC_RCVE1,
-    SYNC_RCVE2,
-    SYNC_SEND,
-    SYNCED,
-    COMMAND
-} rap_state = SYNC_RCVE1;
-
-uint16_t rap_command = RAP_NONE;
-uint16_t rap_sync = 0x0000;
-
-struct
-{
-    enum
-    {
-        WRITE_GET_REG_ID,
-        WRITE_GET_REG_SIZE,
-        WRITE_GET_REG_DATA
-    } state;
-
-    uint16_t reg_id;
-    uint16_t size;
-    uint16_t data[256];
-    uint16_t pos;
-} rap_write_cmd;
 
 void irq_spi2_handler()
 {
-    // Clear interrupt
     NVIC->ICPR[1] |= (0x01 << 4);
 
-    volatile uint16_t sr = SPI2->SR;
+    volatile uint16_t sr = rap.spi->SR;
 
     if ((sr & (0x01 << 3)) || (sr & (0x01 << 6))) // UDR == 1 or OVR == 1
     {
         itm_printf("Error: 0x%08X\n", sr);
 
-        rap_state = SYNC_RCVE1;
-
         // Clear error flag using this software sequence
-        volatile __attribute__((unused)) uint8_t dr = SPI2->DR;
-        sr = SPI2->SR;
+        sr = rap.spi->DR;
+        sr = rap.spi->SR;
         return;
     }
-    
-    if (sr & (0x01 << 0)) // RXNE == 1
+    else if (sr & (0x01 << 0)) // RXNE == 1
     {
-        volatile uint16_t dr = SPI2->DR;
-
-        // itm_printf("%04X\n", dr);
+        volatile uint16_t dr = rap.spi->DR;
+        rap.rx[rap.rx_len++] = dr;
     }
-    else if (sr & (0x01 << 1)) // TXNE == 1
+    else if (sr & (0x01 << 1)) // TXE == 1
     {
-        SPI2->DR = 0x1001;
+        if (rap.tx_len)
+        {
+            rap.spi->DR = rap.tx[rap.tx_ptr++];
+
+            if (rap.tx_ptr == rap.tx_len)
+            {
+                rap.tx_len = 0;
+                rap.tx_ptr = 0;
+            }
+        }
+        else
+            rap.spi->DR = 0x00;
     }
 }
